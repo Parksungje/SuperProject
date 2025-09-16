a351fadcfa12d930151c907f7184fe9d98a2b87e
Parksungje <Parksungje0213@gmail.com>
2025-09-16 09:17:41 +0900

    feat : CharacterDataSO & PlayerEntity

---
+++ Assets/01.Script/Entity/Player/PlayerEntity.cs
@@ -0,0 +1,7 @@
+namespace _01.Script.Entity.Player
+{
+    public abstract class PlayerEntity : Entity
+    {
+        
+    }
+}
---
+++ Assets/01.Script/Entity/SO/CharacterDataSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace _01.Script.Entity.SO
+{
+    [CreateAssetMenu(fileName = "CharacterData", menuName = "SO/Combat/Character Data", order = 0)]
+    public class CharacterDataSO : ScriptableObject
+    {
+        
+    }
+}
