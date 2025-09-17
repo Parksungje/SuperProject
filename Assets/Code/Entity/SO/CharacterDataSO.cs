4ed6e47872c7b2b92fc27240a27f984df4fd2e07
Parksungje <Parksungje0213@gmail.com>
2025-09-17 22:31:13 +0900

    fix : CharacterDataSO

--- Assets/Code/Entity/SO/CharacterDataSO.cs
+++ Assets/Code/Entity/SO/CharacterDataSO.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _01.Script.Entity.SO
 {
     [CreateAssetMenu(fileName = "CharacterData", menuName = "SO/Combat/Character Data", order = 0)]
     public class CharacterDataSO : ScriptableObject
     {
-        
+        public string characterName;
+        public int maxHp;
+        public int attack;
+        public int defense;
+        public int extraAttack; //속공
     }
 }
